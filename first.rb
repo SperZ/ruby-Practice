0dfdd41862924f4f9c5465db75508962921f63a5
SperZ <ZachSperka@gmail.com>
2021-05-10 22:55:16 -0400

    first commit

---
+++ first.rb
@@ -0,0 +1,26 @@
+# declaring a variable and intializing it
+x = 9
+
+# printing variables
+puts(x)
+
+# declaring constants
+MY_NUM = 8
+
+# adding values
+y = MyNum + x
+puts y
+
+# declaring strings
+first_string = 'Hello there'
+puts first_string
+
+# string using escape characters
+
+# getting user input-- gets is the keyword
+
+=begin
+name_input = gets
+hello_name = "Hello ${name_input}"
+print hello_name
+=end
