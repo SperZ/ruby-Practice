af6d3754bcc916208d62c64a8c61b09b520ac1fa
SperZ <ZachSperka@gmail.com>
2021-05-11 22:17:12 -0400

    added string manipulation practice and examples

--- first.rb
+++ first.rb
@@ -40,6 +40,7 @@ else
     puts "nope"
 end
 
+#case statements using case, when, else(similar to default in c#)
 case x
 when 1,2,3
     puts "1-3"
@@ -50,3 +51,32 @@ when 7,8,9
 else
     puts "10 or greater"
 end
+
+
+# string manipulation ---------------------------------------------------------------------------------------------------------
+str = "lace"
+
+str[0] = "m" # index changes the first letter in the string str from l to m
+
+puts str
+
+puts str[0..2] # range feature prints out letter in the str at indexes 0 through 2
+
+puts str[0...2] # range feature prints out lettes in the str at indexes 0 up until 2 but not including 2
+
+str_two = "firetruck"
+puts str.reverse! # reverse the string  in place will also reverse an array,  the ! saves the variable 
+
+puts str
+
+puts str_two.reverse # reverses the str in place but doesnt permanently reverse the string
+
+puts str_two
+
+puts str_two.upcase() # converts the string to all uppercase letters
+
+str_three = "PARTIES"
+
+puts str_three.downcase() # converts the string to all lowercase letters
+
+puts str_three[-1] # gets the last element of the string
